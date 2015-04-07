496a2bff910148813fc5b655e776bd7059ceb1c6
KoltynTemp <kalkomsofwtare@gmail.com>
2015-04-07 14:03:51 -0400

    Update TA.java

--- src/TA.java
+++ src/TA.java
@@ -1,19 +1,10 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author wallk1660
- */
 public class TA {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+                int n1 = 2, n2 = 2, nans;
+        nans = n1*n2;
+        do{
+            system.out.println(""+nans+"");
+        }while(n1>1);
         
         //i think halo is a pretty cool guy he kills and aliens and doesnt afraid of anything
         //Just thought I'd say something
